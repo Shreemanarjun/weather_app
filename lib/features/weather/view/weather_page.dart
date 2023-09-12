6ad3527415c3258e7baff3780e14ca4cc5f542e6
Shreeman Arjun Sahu <shreemanarjunsahu@gmail.com>
2023-09-12 19:07:49 +0530

    ✨ Add weather page

---
+++ lib/features/weather/view/weather_page.dart
@@ -0,0 +1,37 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:weather_app/core/router/router.gr.dart';
+
+@RoutePage(
+  deferredLoading: true,
+)
+class WeatherPage extends StatelessWidget {
+  const WeatherPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AutoTabsRouter.tabBar(
+        routes: const [
+          NextDailyForecastRoute(),
+        ],
+        builder: (context, child, controller) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text("Weather"),
+              leading: const AutoLeadingButton(),
+              bottom: TabBar(
+                controller: controller,
+                tabs: const [
+                  Tab(
+                      text: 'Daily forecast for next 5 days',
+                      icon: Icon(
+                        Icons.event_available,
+                      )),
+                ],
+              ),
+            ),
+            body: child,
+          );
+        });
+  }
+}
